fa71c207d22404cc5f30e801fdb68dca43777402
parssak <parssak@gmail.com>
2022-03-16 10:08:32 -0400

    Clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,12 @@ import { Button, Tooltip } from "demandscience-ui";
 export default function Home() {
   return (
     <>
-      <main className="container min-h-screen grid pt-48 md:pt-0 md:place-items-center">
-        <div className="md:text-center">
+      <main className="container min-h-screen grid place-items-center">
+        <div className="text-center">
           <h1 className="text-3xl font-semibold mb-20">
             TailwindCSS, Next.js & DemandScienceUI Template
           </h1>
-          <Tooltip title="This is a useless button">
+          <Tooltip title="This is a useless button" className="w-max">
             <Button>Hello World</Button>
           </Tooltip>
         </div>
